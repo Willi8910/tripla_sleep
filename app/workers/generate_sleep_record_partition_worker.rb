a4fe5ea64ca2e648387a9f8f5d06cc3ee30e4b7d
William Lie <williamlie8910@gmail.com>
2025-05-04 10:04:31 +0700

    Minor fix worker

--- app/workers/generate_sleep_record_partition_worker.rb
+++ app/workers/generate_sleep_record_partition_worker.rb
@@ -4,14 +4,13 @@ class GenerateSleepRecordPartitionWorker
   include Sidekiq::Worker
 
   def perform(start_date = Date.today)
-    puts start_date
-    # start_date = start_date.beginning_of_month
-    # end_date = (start_date >> 1) # Add 1 month
-    # partition_name = "sleep_records_#{start_date.strftime('%Y_%m')}"
+    start_date = start_date.beginning_of_month
+    end_date = (start_date >> 1) # Add 1 month
+    partition_name = "sleep_records_#{start_date.strftime('%Y_%m')}"
 
-    # ActiveRecord::Base.connection.execute(<<-SQL)
-    #   CREATE TABLE IF NOT EXISTS #{partition_name} PARTITION OF sleep_records
-    #   FOR VALUES FROM ('#{start_date}') TO ('#{end_date}');
-    # SQL
+    ActiveRecord::Base.connection.execute(<<-SQL)
+      CREATE TABLE IF NOT EXISTS #{partition_name} PARTITION OF sleep_records
+      FOR VALUES FROM ('#{start_date}') TO ('#{end_date}');
+    SQL
   end
 end
