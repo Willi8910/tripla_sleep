d379b03e2084f5d2bf5b15dfcfd06afbb3ec73c4
William Lie <williamlie8910@gmail.com>
2025-05-04 10:21:16 +0700

    Following and unfollowing user logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :clock_in, only: %i[create index]
+
+  resources :users, only: [] do
+    member do
+      post 'follow'
+      delete 'unfollow'
+    end
+  end
 end
